62696edba9485e54290048f8aac72c52dc8d36b8
Matthew Cowen <svlnsr3f@students.codeinstitute.net>
2024-12-16 12:04:48 +0000

    GunnderDo Prjoect & GunnerDoList app created

---
+++ gunnerdolist/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((1, "League 1"), (2, "Championship"), (3, "Premiership"))
+PROGRESS = ((1, "Locker Room"), (2, "Kick-off"), (3, "Full-time"))
+
+# Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user"
+    )
+    name = models.CharField(max_length=200)
+    notes = models.TextField(default="Tactics...", null=True, blank=True)
+    in_progress = models.IntegerField(choices=PROGRESS, default=1)
+    importance = models.IntegerField(choices=STATUS, default=2)
+    created_on = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
